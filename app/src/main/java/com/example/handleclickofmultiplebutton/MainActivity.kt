95c0676de874d26287d7c6eb580cfd03b621a1ec
chintan <19bca002@charusat.edu.in>
2022-03-01 17:55:56 +0530

    datepickerdilogbox

--- app/src/main/java/com/example/handleclickofmultiplebutton/MainActivity.kt
+++ app/src/main/java/com/example/handleclickofmultiplebutton/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.handleclickofmultiplebutton
 
+import android.app.DatePickerDialog
+import android.app.SearchManager
 import android.content.DialogInterface
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.DatePicker
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +23,30 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    override fun onBackPressed() {
+
+        var builder=AlertDialog.Builder(this)
+        builder.setTitle("Alert Box")
+        builder.setIcon(R.drawable.ic_launcher_background)
+        builder.setMessage("are you want to exit?")
+        builder.setPositiveButton("yes", DialogInterface.OnClickListener
+        { dialogInterface, i ->
+            Toast.makeText(this,"good bye",Toast.LENGTH_SHORT).show()
+            finish()
+        })
+        builder.setNegativeButton("no", DialogInterface.OnClickListener { dialogInterface, i ->
+
+            Toast.makeText(this,"ohh! welcome back",Toast.LENGTH_SHORT).show()
+
+        })
+
+        var dialog=builder.create()
+        dialog.show()
+
+
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
     fun onclick(view: View) {
         if(view.id==R.id.btnAlert)
         {
@@ -39,17 +69,59 @@ class MainActivity : AppCompatActivity() {
             builder.setIcon(R.drawable.ic_launcher_background)
 
             //create alert dialog box
-            var Alert=builder.create()
+            //var Alert=builder.create()
+            builder.create()
 
             //specifi complasury select one option than leave the dialog box
-            Alert.setCancelable(false)
-
+           // Alert.setCancelable(false)
+            builder.setCancelable(false)
             //display the dialog box
-            Alert.show()
+            //Alert.show()
+            builder.show()
         }
         else if(view.id==R.id.btnsecond)
         {
             Toast.makeText(this,"second btn click",Toast.LENGTH_SHORT).show()
+        }
+        else if(view.id==R.id.edtdate)
+        {
+            var cal:Calendar= Calendar.getInstance()
+
+            var Date=cal.get(Calendar.DAY_OF_MONTH)
+            var year=cal.get(Calendar.YEAR)
+            var month=cal.get(Calendar.MONTH)
+
+            var date:DatePickerDialog= DatePickerDialog(this,DatePickerDialog.OnDateSetListener(
+                { DatePickerDialog, year, month, date ->
+                    edtdate.setText("$date/$month/$year")
+                }
+            ),year,month,Date)
+            date.show()
+        }
+
+        else if(view.id==R.id.btndatepicker)
+        {
+            var cal:Calendar= Calendar.getInstance()
+
+            var Date=cal.get(Calendar.DAY_OF_MONTH)
+            var year=cal.get(Calendar.YEAR)
+            var month=cal.get(Calendar.MONTH)
+
+
+
+
+
+            var date:DatePickerDialog= DatePickerDialog(this,DatePickerDialog.OnDateSetListener(
+                { DatePickerDialog, year, month, date ->
+
+                        Toast.makeText(this, "$date/${month + 1}/$year", Toast.LENGTH_SHORT).show()
+                        edtdate.setText("$date/${month + 1}/$year")
+
+                }
+            ),year,month,Date)
+            date.show()
+
+
         }
     }
 }
